39a050ea20e5bf1a69aea5403658d34fb75fcd25
Ryan <r.walter215@gmail.com>
2017-09-23 12:40:06 -0500

    styled intro a bit

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,25 +1,36 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Grid, Image} from 'semantic-ui-react'
 import style from './Intro.css';
 
 class Intro extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
 
     this.pic = '../../../public/test.jpg'
   }
   render() {
     return (
-      <div style={style}>
-      <h1 className='intro__header'>Carla Cino</h1>
-        <img src='http://engineering.unl.edu/images/staff/Kayla_Person-small.jpg' className="circular--portrait"/>
-        <div>
-          <h3 className='intro__description'>Writer</h3>
-            <div className='intro__description intro__divider'> . </div>
-          <h3 className='intro__description'>French</h3>
-            <div className='intro__description intro__divider'> . </div>
-          <h3 className='intro__description'>Go Away</h3>
-        </div>
-      </div>
+      <Grid style={style} textAlign='center' verticalAlign='center' centered columns={1}>
+        <Grid.Column>
+          <h1 className='intro__header'>Carla Cino</h1>
+        </Grid.Column>
+        <Grid.Column>
+          <img src='http://engineering.unl.edu/images/staff/Kayla_Person-small.jpg' className="circular--portrait"/>
+        </Grid.Column>
+        <Grid.Column>
+          <div>
+            <h3 className='intro__description'>Writer</h3>
+            <div className='intro__description intro__divider'>
+              .
+            </div>
+            <h3 className='intro__description'>French</h3>
+            <div className='intro__description intro__divider'>
+              .
+            </div>
+            <h3 className='intro__description'>Go Away</h3>
+          </div>
+        </Grid.Column>
+      </Grid>
     );
   }
 }
