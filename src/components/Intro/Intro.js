bb2f0caec621d42e6f06ca243a490c7c55f49bb7
Ryan <r.walter215@gmail.com>
2017-09-26 11:57:50 -0500

    completed home page, imported google fonts

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -11,25 +11,29 @@ class Intro extends Component {
   render() {
     return (
       <Grid style={style} textAlign='center' verticalAlign='center' centered columns={1}>
-        <Grid.Column>
+
+        <Grid.Row centered columns={2}>
           <h1 className='intro__header'>Carla Cino</h1>
-        </Grid.Column>
-        <Grid.Column>
-          <img src='http://engineering.unl.edu/images/staff/Kayla_Person-small.jpg' className="circular--portrait"/>
-        </Grid.Column>
-        <Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row centered columns={2}>
+          <img src='carla.jpg' className="circular--portrait"/>
+        </Grid.Row>
+
+        <Grid.Row centered columns={2}>
           <div>
+            <h3 className='intro__description'>Journalist</h3>
+            <div className='intro__description intro__divider'>
+               |
+            </div>
+            <h3 className='intro__description'>Copywriter</h3>
+            <div className='intro__description intro__divider'>
+               |
+            </div>
             <h3 className='intro__description'>Writer</h3>
-            <div className='intro__description intro__divider'>
-              .
-            </div>
-            <h3 className='intro__description'>French</h3>
-            <div className='intro__description intro__divider'>
-              .
-            </div>
-            <h3 className='intro__description'>Go Away</h3>
           </div>
-        </Grid.Column>
+        </Grid.Row>
+
       </Grid>
     );
   }
