bcbdd84afaa1aa442530b25a806bb1390dd7dcc5
Ryan <r.walter215@gmail.com>
2017-09-30 20:03:59 -0500

    created contact page

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -4,6 +4,7 @@ import {
   Route,
   Link
 } from 'react-router-dom'
+import {Grid, Image, Header, Container, Segment, Form, Button, TextArea} from 'semantic-ui-react'
 import style from './Contact.css'
 
 export default class Contact extends Component {
@@ -12,7 +13,28 @@ export default class Contact extends Component {
   render() {
 
     return (
-      <h1>Contact Me</h1>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={3}>
+          </Grid.Column>
+
+          <Grid.Column width={10}>
+            <Header size='huge'>Contact Me</Header>
+            <Segment className='form' inverted>
+              <Form inverted>
+                <Form.Group widths='equal'>
+                  <Form.Input label='Name' placeholder='Name' />
+                  <Form.Input label='Subject' placeholder='Subject' />
+                </Form.Group>
+                <TextArea autoHeight placeholder='What can I do for you?' style={{ minHeight: 100 }} />
+                <Button type='submit'>Submit</Button>
+              </Form>
+            </Segment>
+          </Grid.Column>
+
+        </Grid.Row>
+
+      </Grid>
     )
   }
 }
