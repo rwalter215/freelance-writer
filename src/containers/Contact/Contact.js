52d8c477eba2e982cb18bb245a1ee107a4083220
Ryan <r.walter215@gmail.com>
2017-10-07 18:27:24 -0500

    created a server

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -1,33 +1,59 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react';
+import axios from 'axios';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
-import {Grid, Image, Header, Container, Segment, Form, Button, TextArea} from 'semantic-ui-react'
+  Grid,
+  Image,
+  Header,
+  Container,
+  Segment,
+  Form,
+  Button,
+  TextArea
+} from 'semantic-ui-react';
 import style from './Contact.css'
 
 export default class Contact extends Component {
-  state = {}
+  state = {
+    name: '',
+    emailAddress: '',
+    emailSubject: '',
+    emailContent: ''
+  }
+
+  handleChange = (e, {name, value}) => this.setState({[name]: value})
+
+  handleSubmit = () => {
+    console.log('state: ', this.state)
+
+    axios.post('/contact', {
+      email: 'r.walter215@gmail.com',
+      name: 'Ryan Walter',
+      message: 'No! Go Away!'
+    }).then(function(response) {
+      console.log(response);
+    }).catch(function(error) {
+      console.log(error);
+    });
+  }
 
   render() {
-
+    const {name, emailAddress, emailSubject, emailContent} = this.state;
     return (
       <Grid>
         <Grid.Row>
-          <Grid.Column width={3}>
-          </Grid.Column>
+          <Grid.Column width={3}></Grid.Column>
 
           <Grid.Column width={10}>
             <Header size='huge'>Contact Me</Header>
             <Segment className='form' inverted>
-              <Form inverted>
+              <Form onSubmit={this.handleSubmit} inverted>
                 <Form.Group widths='equal'>
-                  <Form.Input label='Name' placeholder='Name' />
-                  <Form.Input label='Email' placeholder='Email' />
+                  <Form.Input label='Name' placeholder='Name' name='name' value={name} onChange={this.handleChange}/>
+                  <Form.Input label='Email' placeholder='Email' name='emailAddress' value={emailAddress} onChange={this.handleChange}/>
                 </Form.Group>
-                <Form.Input label='Subject' placeholder='Subject' />
-                <TextArea autoHeight placeholder='What can I do for you?' style={{ minHeight: 100 }} />
+                <Form.Input label='Subject' placeholder='Subject' name='emailSubject' value={emailSubject} onChange={this.handleChange}/>
+                <TextArea autoHeight placeholder='What can I do for you?' name='emailContent' value={emailContent} onChange={this.handleChange} style={{ minHeight: 100 }}/>
                 <Button className='button--submit' type='submit'>Submit</Button>
               </Form>
             </Segment>
